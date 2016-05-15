2142ad87d17f6dc14ed60c1ed2f0fe7722ef9da2
l2el3er <se56160134@gmail.com>
2016-05-15 22:46:34 +0700

    Rejected commit: Default

--- app/src/main/java/com/example/gunka/kujapom/FeedTwoFragment.java
+++ app/src/main/java/com/example/gunka/kujapom/FeedTwoFragment.java
@@ -1,9 +1,84 @@
 package com.example.gunka.kujapom;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by gunka on 28-Apr-16.
  */
 public class FeedTwoFragment extends Fragment {
+
+    CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private TextView info;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+
+        View v = inflater.inflate(R.layout.facebook_login, container, false);
+
+        AppEventsLogger.activateApp(getActivity());
+        callbackManager = CallbackManager.Factory.create();
+        info = (TextView) v.findViewById(R.id.info);
+        loginButton = (LoginButton) v.findViewById(R.id.login_button);
+        loginButton.setFragment(this);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object,GraphResponse response) {
+                                try {
+                                    info.setText("Hi, " + object.getString("name"));
+                                } catch(JSONException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
